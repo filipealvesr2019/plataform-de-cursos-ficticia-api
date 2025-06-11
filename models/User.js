29014c7d6e9bed6a04491b052639da17bd3ed4d8
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-11 14:35:57 -0300

    feat: adiciona models

--- models/User.js
+++ models/User.js
@@ -8,6 +8,15 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.method.comparePassword = function (senha) {
+  return bcrypt.compare(senha, this.password)
+}
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
