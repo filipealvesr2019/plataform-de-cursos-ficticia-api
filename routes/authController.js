c72561c5f6106afadb575dc4bfed6fc8e1cad710
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-17 15:22:37 -0300

    feat: adiciona models

--- routes/authController.js
+++ routes/authController.js
@@ -1,2 +1,15 @@
 const User = require('../models/User');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+
+const gerarToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { inspiresIn: '7d'});
+
+exports.register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try{
+        const usuario = new User({ name, email, password});
+    }catch(error){
+        console.log(error);
+    }
+
+}
